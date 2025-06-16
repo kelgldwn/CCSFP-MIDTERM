4e59fae79c00922271856c4ae58f256c19292d7b
kelgldwn <kelmarvill8@gmail.com>
2025-06-17 01:26:28 +0800

    agent

--- dashboard/agent/authentication/agent-class.php
+++ dashboard/agent/authentication/agent-class.php
@@ -106,7 +106,7 @@ public function systemLogo(){
   try
   {
    $stmt = $this->conn->prepare("SELECT * FROM users WHERE email=:email_id AND account_status = :account_status AND user_type = :user_type");
-   $stmt->execute(array(":email_id"=>$email , ":account_status" => "active", "user_type" => 2));
+   $stmt->execute(array(":email_id"=>$email , ":account_status" => "active", ":user_type" => 2));
    $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
    
 
